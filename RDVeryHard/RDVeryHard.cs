0481c3580451020b8033800044c490f824c82f7b
RandomGuyJCI <carleisaguirre@gmail.com>
2022-04-10 01:39:13 +0800

    Allow enabling Very Hard for individual players

--- RDVeryHard/RDVeryHard.cs
+++ RDVeryHard/RDVeryHard.cs
@@ -1,36 +1,57 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
 
 namespace RDVeryHard
 {
-    [BepInPlugin("com.rhythmdr.veryhard", "Very Hard Difficulty for Rhythm Doctor", "1.0.0")]
+    [BepInPlugin("com.rhythmdr.veryhard", "Very Hard Difficulty for Rhythm Doctor", "1.1.0")]
     [BepInProcess("Rhythm Doctor.exe")]
     public class RDVeryHard : BaseUnityPlugin
     {
-        private ConfigEntry<bool> configEnableVeryHard;
+        private static ConfigEntry<VeryHardMode> configVeryHardMode;
+
+        private enum VeryHardMode
+        {
+            None,
+            P1,
+            P2,
+            Both,
+        }
         void Awake()
         {
-            configEnableVeryHard = Config.Bind("General", "EnableVeryHard", true,
-                "Enables Very Hard difficulty. Not affected by the difficulty setting in Rhythm Doctor when enabled.");
+            configVeryHardMode = Config.Bind("General", "VeryHardMode", VeryHardMode.Both,
+                "Sets the player(s) in which Very Hard difficulty is enabled. Not affected by the difficulty setting in Rhythm Doctor when enabled.");
 
-            if (configEnableVeryHard.Value)
+            switch (configVeryHardMode.Value)
             {
-                Harmony.CreateAndPatchAll(typeof(EnableVeryHard));
+                case VeryHardMode.P1:
+                case VeryHardMode.P2:
+                case VeryHardMode.Both:
+                    Harmony.CreateAndPatchAll(typeof(VeryHard));
+                    break;
+                case VeryHardMode.None:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
 
             Logger.LogMessage("Plugin enabled!");
         }
-    }
 
-    public static class EnableVeryHard
-    {
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(scnGame), "GetHitMargin")]
-        public static bool Prefix(ref float __result)
+        public static class VeryHard
         {
-            __result = 0.025f;
-            return false;
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(scnGame), "GetHitMargin")]
+            public static void Postfix(RDPlayer player, ref float __result)
+            {
+                if ((configVeryHardMode.Value == VeryHardMode.Both) ||
+                    (player == RDPlayer.P1 && configVeryHardMode.Value == VeryHardMode.P1) ||
+                    (player == RDPlayer.P2 && configVeryHardMode.Value == VeryHardMode.P2))
+                {
+                    __result = 0.025f;
+                }
+            }
         }
     }
 }
